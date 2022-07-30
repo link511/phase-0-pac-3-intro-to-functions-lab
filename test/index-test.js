dd1e35c9bfe70eec751dc1af4d25c8a9669661ba
Lincoln <lincoln@taylorlivingco.com>
2022-07-30 17:33:21 +1000

    complete assignment

--- test/index-test.js
+++ test/index-test.js
@@ -35,12 +35,17 @@ describe('logWhisper(string)', function() {
   })
 })
 
-describe('sayHiToGrandma(string)', function() {
-  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
+describe('sayHiToGrandma(string)', function() 
+
+{
+  it('returns "I can\'t hear you!" if `string` is lowercase', function() 
+  
+  {
     expect(sayHiToGrandma('hello')).toEqual("I can't hear you!");
   })
 
-  it('returns "YES INDEED!" if `string` is uppercase', function() {
+  it('returns "YES INDEED!" if `string` is uppercase', function() 
+  {
     expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!");
   })
 
